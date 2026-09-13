bab41c8366e6b2ec8aa0ee8bcd23790ba501c241
Mian Iqbal <iqbalmian043@gmail.com>
2026-09-13 10:19:21 +0500

    Add Monetag service worker and verification files

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "5gvci.com",
-    "zoneId": 11788178
+    "zoneId": 11788917
 }
 self.lary = ""
 importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
